821b87143fc4ecd3bafb191b7085ed37567bba1f
Alan McGrath <alan.j.mcgrath@mycit.ie>
2018-04-23 14:43:29 +0100

    Create HelloThere.java

---
+++ src/ie/cit/agile/HelloThere.java
@@ -0,0 +1,12 @@
+package ie.cit.agile;
+
+public class HelloThere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("General Kenobi!!");
+		System.out.println("-----------------------------");
+	}
+
+}
